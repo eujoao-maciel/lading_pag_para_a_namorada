fd75ddf254cda4b81bc86fb2d4c50dda1c0e5cd2
joao eudes <joao.eudes13jem@gmail.com>
2024-11-07 05:44:24 -0300

    criado logica do menu lateral

---
+++ src/js/scripts.js
@@ -0,0 +1,18 @@
+const btnMenu = document.getElementById('btn_menu')
+
+const addMenu = () => {
+    const aside = document.createElement('aside')
+    aside.classList.add('menu-add')
+    document.querySelector('body').appendChild(aside)
+    
+    const handleClickOutSide = (evt) => {
+        if (!aside.contains(evt.target) && evt.target !== btnMenu) {
+            aside.remove()
+            document.removeEventListener('click', handleClickOutSide);
+        }
+    }
+
+    document.addEventListener('click', handleClickOutSide)
+}
+
+btnMenu.addEventListener('click', addMenu)
