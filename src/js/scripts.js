bdf23fd496b0dae58a5938aa417c3f8869918270
joao eudes <joao.eudes13jem@gmail.com>
2024-11-08 03:41:28 -0300

    adicionado topicos da nav do botao lateral

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,6 +4,13 @@ const addMenu = () => {
     const aside = document.createElement('aside')
     aside.classList.add('menu-add')
     document.querySelector('body').appendChild(aside)
+    aside.innerHTML = `
+            <ul class="navigation">
+                <li>Sobre</li>
+                <li>Hobbies</li>
+                <li>Pets</li>
+                <li>Contatos</li>
+            </ul>`
     
     const handleClickOutSide = (evt) => {
         if (!aside.contains(evt.target) && evt.target !== btnMenu) {
